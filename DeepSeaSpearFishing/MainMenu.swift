e07497920c770e0f820f12c47f6b4d25a4df0401
Alejandro Casanas <casanas10@gmail.com>
2017-05-03 16:49:10 -0400

    Changes made

---
+++ DeepSeaSpearFishing/MainMenu.swift
@@ -0,0 +1,13 @@
+//
+//  MainMenu.swift
+//  DeepSeaSpearFishing
+//
+//  Created by alejandro casanas on 4/24/17.
+//  Copyright © 2017 AlejandroCasanas. All rights reserved.
+//
+
+import SpriteKit
+
+class MainMenu: SKScene {
+
+}
